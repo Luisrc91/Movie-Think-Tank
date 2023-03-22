493e5bda5c480a973759f61b213d6e23945a5cb0
Luis <luisrc91@live.com>
2023-03-22 18:57:29 -0400

    Movie-think-tank

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import DisplayIdeas from "./components/DisplayIdeas";
+import IdeasForm from "./components/IdeasForm";
+
+function App() {
+  const [ideas, setIdeas] = useState([
+    "Movie about a guy who gets bit by a radioactive mole rat",
+    "Romcom about two developers over Zoom",
+    "Horror film where AWS goes down for 24 hours",
+  ]);
+
+  const addIdeas = (newIdeas) => {
+    setIdeas([...ideas, newIdeas]);
+  };
+
+  return (
+    <main>
+      <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
+
+      <DisplayIdeas ideas={ideas} />
+      <h1>Submit your ideas down below</h1>
+      <IdeasForm addIdeas={addIdeas} />
+    </main>
+  );
+}
+
+export default App;
